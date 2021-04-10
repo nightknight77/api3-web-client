0c408e2336223a419b16c1738ea6526aa63f015c
gwn <dev@gwn.wtf>
2021-04-11 02:31:01 +0300

    Implement deposit

--- src/scenes/PoolManager.js
+++ src/scenes/PoolManager.js
@@ -1,11 +1,24 @@
 const
-    {createElement} = require('react'),
+    {createElement, useState} = require('react'),
     {useWeb3} = require('lib/web3'),
-    {Card} = require('lib/ui')
+    {Card} = require('lib/ui'),
+    {POOL_CONTRACT_ADDR} = process.env
+
+
+const deposit = async (amount, web3) => {
+    const approveResp =
+        await web3.contracts.token.approve(POOL_CONTRACT_ADDR, amount)
+
+    approveResp.wait()
+
+    await web3.contracts.pool.deposit(web3.account, amount, web3.account)
+}
 
 
 const PoolManager = () => {
-    const web3 = useWeb3()
+    const
+        web3 = useWeb3(),
+        [depositAmount, setDepositAmount] = useState('')
 
     return <Card>
         <h2 children='Pool' />
@@ -14,6 +27,21 @@ const PoolManager = () => {
             <li>Deposited: {web3.depositAmount.toString()}</li>
             <li>Staked: {web3.stakeAmount.toString()}</li>
         </ul>
+
+        <p>
+            <input
+                type='number'
+                value={depositAmount}
+                placeholder='100'
+                onChange={e => setDepositAmount(e.target.value)}
+                style={{width: 50}}
+            />
+
+            <button
+                children='Deposit'
+                onClick={() => deposit(depositAmount, web3)}
+            />
+        </p>
     </Card>
 }
 
