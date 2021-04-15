7d23ed3f56b60bba768eb23a333b0ee40d698ca6
gwn <dev@gwn.wtf>
2021-04-15 17:59:41 +0300

    Improve the Input component

--- src/lib/ui/Input.js
+++ src/lib/ui/Input.js
@@ -1,15 +1,18 @@
 const {createElement} = require('react')
 
 
-const Input = ({type, style: extraStyles, ...props}) =>
+const Input = ({type, style: extraStyles, size = 'md', ...props}) =>
     <input
         style={{
-            padding: 10,
+            padding: '10px 0',
             margin: 5,
-            border: '1px solid #777',
+            border: '0 solid #777',
+            borderBottomWidth: 1,
             color: '#777',
+            outline: 0,
+            fontSize: {sm: '0.8em', md: '1em', lg: '1.5em'}[size],
             backgroundColor: 'transparent',
-            width: 80,
+            width: 70,
 
             ...(type === 'number' && {
                 fontWeight: 'bold',
