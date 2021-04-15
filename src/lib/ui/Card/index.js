cd47d8fecc8b31e8f2115320adc4f76aca984eb9
gwn <dev@gwn.wtf>
2021-04-15 19:09:23 +0300

    Update the Card component according to the design
    Add title and call to action parameters and style them properly.

--- src/lib/ui/Card/index.js
+++ src/lib/ui/Card/index.js
@@ -1,15 +1,41 @@
 const
     {createElement} = require('react'),
+    Button = require('../Button'),
     s = require('./style.css').default
 
 
-const Card = ({title, children, className = '', ...props}) =>
+const Card = ({
+    title,
+    children,
+    cta1: {title: cta1Title, action: cta1Action} = {},
+    cta2: {title: cta2Title, action: cta2Action} = {},
+    className = '',
+    ...props
+}) =>
     <div
         className={s.card + ' ' + className}
         {...props}
     >
-        {title && <h2 children={title}/>}
+        <header>
+            <h2 children={title} />
+
+            <Button
+                children={cta1Title}
+                onClick={cta1Action}
+                style={{visibility: cta1Title ? 'visible' : 'hidden'}}
+            />
+        </header>
+
         {children}
+
+        <footer>
+            {cta2Title &&
+                <Button
+                    children={cta2Title}
+                    onClick={cta2Action}
+                    className={s.button}
+                />}
+        </footer>
     </div>
 
 
