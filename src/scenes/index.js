a8dec6b9d702954018e8811b29c2663fdb9fa56c
gwn <dev@gwn.wtf>
2021-05-12 15:47:40 +0300

    Refactor the scene directory for clarity
    Put sub scenes into the relevant scene directories.

--- src/scenes/index.js
+++ src/scenes/index.js
@@ -1,7 +1,2 @@
 export {default as Staking} from './Staking'
-export {default as WalletManager} from './WalletManager'
-export {default as Balance} from './Balance'
-export {default as StakingStats} from './StakingStats'
-export {default as Unstaking} from './Unstaking'
-export {default as StakingPool} from './StakingPool'
 export {default as ProposalListing} from './ProposalListing'
